2b21721a3d6805252b7f40734b70bfc710768893
TonyB-224 <tbullard224@gmail.com>
2025-10-25 16:50:47 -0500

    App Dev 1

---
+++ MAD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MAD
+//
+//  Created by user942908 on 10/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
